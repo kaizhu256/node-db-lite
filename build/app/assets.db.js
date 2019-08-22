7aae9e0b49bb1a4bb213a5e4826c15ed7f2175df
Travis CI User <travis@example.org>
2019-08-22 04:12:30 +0000

    [ci skip] update gh-pages

--- build/app/assets.db.js
+++ build/app/assets.db.js
@@ -1,6 +1,6 @@
 // usr/bin/env node
 /*
- * lib.db.js (2018.12.31)
+ * lib.db.js (2019.8.20)
  * https://github.com/kaizhu256/node-db-lite
  * this zero-dependency package will provide a persistent, in-browser database, with a working web-demo
  *
